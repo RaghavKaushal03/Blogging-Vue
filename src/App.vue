063dd4f80b28fb114ac1c508aa28a58d56b444fd
RaghavKaushal03 <kaushal.raghav13@gmail.com>
2021-10-02 18:38:43 +0530

    till 3:26

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,50 @@
 <template>
   <div class="app-wrapper">
-    <div class="app">
-      <Navigation v-if="!navigation"/>
+    <div class="app" v-if="this.$store.state.postLoaded">
+      <Navigation v-if="!navigation" />
       <router-view />
-      <Footer v-if="!navigation"/>
+      <Footer v-if="!navigation" />
     </div>
   </div>
 </template>
 
 <script>
-import Navigation from './components/Navigation.vue'
-import Footer from './components/Footer.vue'
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import firebase from "firebase/app";
+import "firebase/auth";
 export default {
   name: "app",
-  components: {
-    Navigation,
-    Footer,
-  },
+  components: { Navigation, Footer },
   data() {
     return {
       navigation: null,
     };
   },
   created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.$store.commit("updateUser", user);
+      if (user) {
+        this.$store.dispatch("getCurrentUser", user);
+      }
+    });
     this.checkRoute();
+    this.$store.dispatch("getPost");
   },
   mounted() {},
   methods: {
     checkRoute() {
-      if(this.$route.name === "Login" || this.$route.name === "Register" || this.$route.name === "ForgotPassword" )
-      {
+      if (this.$route.name === "Login" || this.$route.name === "Register" || this.$route.name === "ForgotPassword") {
         this.navigation = true;
         return;
       }
       this.navigation = false;
-    }
+    },
   },
   watch: {
     $route() {
       this.checkRoute();
-    }
+    },
   },
 };
 </script>
